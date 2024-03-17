cf2151a384619d3faf11b5118f896522f84ac5ea
Heimatseiten <159701446+Heimatseiten@users.noreply.github.com>
2024-03-17 18:55:48 +0100

    added moment and added JS to show current time and date of location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  //Hawai
+  let cologneElement = document.querySelector("#hawai");
+  let cologneDateElement = cologneElement.querySelector(".date");
+  let cologneTimeElement = cologneElement.querySelector(".time");
+  let cologneTime = moment().tz("Pacific/Honolulu");
 
+  cologneDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  cologneTimeElement.innerHTML = cologneTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+
+  //Iceland
+  let romElement = document.querySelector("#iceland");
+  let romDateElement = romElement.querySelector(".date");
+  let romTimeElement = romElement.querySelector(".time");
+  let romTime = moment().tz("Iceland");
+
+  romDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  romTimeElement.innerHTML = romTime.format("h:mm:ss[<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
