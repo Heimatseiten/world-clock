ad493abdc9c60ad0164a5f91133d9fb89817674e
Heimatseiten <159701446+Heimatseiten@users.noreply.github.com>
2024-03-17 22:00:14 +0100

    final CSS adjustment + adding more cities

--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
 function updateTime() {
   //Hawai
-  let cologneElement = document.querySelector("#hawai");
-  let cologneDateElement = cologneElement.querySelector(".date");
-  let cologneTimeElement = cologneElement.querySelector(".time");
-  let cologneTime = moment().tz("Pacific/Honolulu");
+  let hawaiElement = document.querySelector("#hawai");
+  let hawaiDateElement = hawaiElement.querySelector(".date");
+  let hawaiTimeElement = hawaiElement.querySelector(".time");
+  let hawaiTime = moment().tz("Pacific/Honolulu");
 
-  cologneDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  cologneTimeElement.innerHTML = cologneTime.format(
-    "h:mm:ss[<small>]A[</small>]"
-  );
+  hawaiDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  hawaiTimeElement.innerHTML = hawaiTime.format("h:mm:ss [<small>]A[</small>]");
 
   //Iceland
-  let romElement = document.querySelector("#iceland");
-  let romDateElement = romElement.querySelector(".date");
-  let romTimeElement = romElement.querySelector(".time");
-  let romTime = moment().tz("Iceland");
+  let icelandElement = document.querySelector("#iceland");
+  let icelandDateElement = icelandElement.querySelector(".date");
+  let icelandTimeElement = icelandElement.querySelector(".time");
+  let icelandTime = moment().tz("Iceland");
 
-  romDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  romTimeElement.innerHTML = romTime.format("h:mm:ss[<small>]A[</small>]");
+  icelandDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  icelandTimeElement.innerHTML = icelandTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  //Maledives
+  let maledivesElement = document.querySelector("#maledives");
+  let maledivesDateElement = maledivesElement.querySelector(".date");
+  let maledivesTimeElement = maledivesElement.querySelector(".time");
+  let maledivesTime = moment().tz("Indian/Maldives");
+
+  maledivesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  maledivesTimeElement.innerHTML = maledivesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  //New Zealand
+  let newZealandElement = document.querySelector("#newZealand");
+  let newZealandDateElement = newZealandElement.querySelector(".date");
+  let newZealandTimeElement = newZealandElement.querySelector(".time");
+  let newZealandTime = moment().tz("Indian/Maldives");
+
+  newZealandDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  newZealandTimeElement.innerHTML = newZealandTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 }
 
 function updateCity(event) {
