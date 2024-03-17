e40ed6003f5002efff73209c1d7a87216602b5b9
Heimatseiten <159701446+Heimatseiten@users.noreply.github.com>
2024-03-17 19:56:50 +0100

    added html structure for current time selector, added select city js function

--- src/index.js
+++ src/index.js
@@ -20,5 +20,26 @@ function updateTime() {
   romTimeElement.innerHTML = romTime.format("h:mm:ss[<small>]A[</small>]");
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+        <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small></div>
+        </div>
+    `;
+}
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
+//cityTimeZone.replace("_", " ").split("/")[1];
